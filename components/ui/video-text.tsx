afac222327bb5fc9afbfdf419e96289f5ec74a9e
jatinnathh <jatinnath1111@gmail.com>
2026-07-28 15:05:04 +0530

    fixed bugs in vercel deploymeny

--- components/ui/video-text.tsx
+++ components/ui/video-text.tsx
@@ -62,7 +62,7 @@ export interface VideoTextProps {
    * The element type to render for the text
    * @default "div"
    */
-  as?: React.ElementType
+  as?: React.ElementType<any>
 }
 
 export function VideoText({
